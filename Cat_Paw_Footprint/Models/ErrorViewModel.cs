315743021c69ab87ec0958784dfe6fd2efe170a7
THR0209 <76144817+THR0209@users.noreply.github.com>
2025-08-29 13:02:03 +0800

    Delete Cat_Paw_Footprint directory

--- Cat_Paw_Footprint/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cat_Paw_Footprint.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
