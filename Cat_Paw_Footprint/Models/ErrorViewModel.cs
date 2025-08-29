6827764f3e5673f9f07ebc4f7c38496ddee61141
Eden <l7w3gcww@gmail.com>
2025-08-29 18:39:23 +0800

    000

---
+++ Cat_Paw_Footprint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cat_Paw_Footprint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
