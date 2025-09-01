61a00996dd779b5579b866cb07b77b7e9d5c0cd8
chouyi <space6234@gmail.com>
2025-09-01 16:29:18 +0800

    202509011628 第一次commit 周弈

---
+++ testDBConnection/testDBConnection/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using testDBConnection.Models;
+
+namespace testDBConnection.Controllers
+{
+	public class TestController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		private readonly MyDbContext _context;
+
+		public TestController(MyDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult CheckDb()
+		{
+			try
+			{
+				bool canConnect = _context.Database.CanConnect();
+				return Content(canConnect ? "✅ 資料庫連線成功！" : "❌ 無法連線資料庫");
+			}
+			catch (Exception ex)
+			{
+				return Content("錯誤：" + ex.Message);
+			}
+		}
+	}
+}
