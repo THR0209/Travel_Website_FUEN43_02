aad2be73c3e3c378db325cc28a83aa73cdd4f66b
chouyi <space6234@gmail.com>
2025-09-12 14:54:52 +0800

    新增報表controller 修改supadmin 導覽列

---
+++ Cat_Paw_Footprint/Areas/Admin/Controllers/ReportsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cat_Paw_Footprint.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ReportsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
