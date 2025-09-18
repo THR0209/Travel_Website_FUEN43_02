2fa8ce89226a2e67589b4b32c1881d41ae0d2243
chi <iwanchi1023@gmail.com>
2025-09-18 14:56:18 +0800

    chi push
    增加頁面標題登入
    調整_Logout
    site.css新增 sidebar樣式、card樣式
    core.css新增 layout-menu樣式、讓 footer 固定在底部(html, body、.layout-wrapper、.layout-page、.content-wrapper、footer)
    介面調整
    改掉btn顏色
    EmployeeAuthController新增產生 Claims
    CustomerSupportTickets新增工單改為下拉及輸入關鍵字可搜尋到客戶
    處理人員改為顯示目前登入員工、編輯改為只有類型和狀態可以更改、ID改為工單編號
    appsettings.json新增"Pass": "dtww iwmw jggn cguo"

--- Cat_Paw_Footprint/wwwroot/js/site.js
+++ Cat_Paw_Footprint/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+(() => {
+    // ===============================
+    // 顯示/隱藏按鈕
+    // ===============================
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) { $('#backToTop').fadeIn(); }
+        else { $('#backToTop').fadeOut(); }
+    });
+
+    // ===============================
+    // 點擊回到頂部
+    // ===============================
+    $('#backToTop').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 200);
+    });
+})();
