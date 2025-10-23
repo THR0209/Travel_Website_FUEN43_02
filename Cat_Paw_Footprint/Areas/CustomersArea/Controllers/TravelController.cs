44b0ec9cd89417965f528e4208279f7c33564d61
Eden <l7w3gcww@gmail.com>
2025-10-23 09:33:17 +0800

    + 跳轉連結

---
+++ Cat_Paw_Footprint/Areas/CustomersArea/Controllers/TravelController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cat_Paw_Footprint.Areas.CustomersArea.Controllers
+{
+	[Area("CustomersArea")]
+	[AllowAnonymous]
+	public class TravelController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
